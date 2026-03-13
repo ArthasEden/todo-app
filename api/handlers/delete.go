7d54e32707693bc72d7dc6727f0e1e5a65b46cc6
Rusyaev S.S. <arthas.eden@gmail.com>
2026-03-13 17:43:58 +0600

    add handlers Delete and Patch

--- api/handlers/delete.go
+++ api/handlers/delete.go
@@ -1,5 +1,23 @@
 package handlers
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+	"todo-app/api/dto"
+	"todo-app/service"
 
-func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) {}
+	"github.com/gorilla/mux"
+)
+
+func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) {
+	title := mux.Vars(r)["title"]
+
+	if err := h.list.Delete(title); err != nil {
+		if errors.Is(err, service.ErrTaskNotFound) {
+			http.Error(w, dto.ErrDTO(err), http.StatusBadRequest)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
--- api/handlers/patch.go
+++ api/handlers/patch.go
@@ -1,5 +1,40 @@
 package handlers
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+	"todo-app/api/dto"
+	"todo-app/service"
 
-func (h *Handler) Patch(w http.ResponseWriter, r *http.Request) {}
+	"github.com/gorilla/mux"
+)
+
+func (h *Handler) Patch(w http.ResponseWriter, r *http.Request) {
+	var (
+		title  = mux.Vars(r)["title"]
+		status = r.URL.Query().Get("completed")
+		err    error
+		task   service.Task
+	)
+
+	switch status {
+	case "true":
+		task, err = h.list.Complete(title)
+	case "false":
+		task, err = h.list.Uncomplete(title)
+	default:
+		http.Error(w, dto.ErrDTO(errors.New("invalid completed parameter")), http.StatusBadRequest)
+		return
+	}
+
+	if err != nil {
+		if errors.Is(err, service.ErrTaskNotFound) {
+			http.Error(w, dto.ErrDTO(err), http.StatusNotFound)
+			return
+		}
+		http.Error(w, dto.ErrDTO(err), http.StatusInternalServerError)
+		return
+	}
+
+	SendJSON(w, task, http.StatusOK)
+}

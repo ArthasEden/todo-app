2ec009a875ad482a4908f6b275cec10276400d54
Rusyaev S.S. <arthas.eden@gmail.com>
2026-03-17 11:54:02 +0500

    delete

--- main.go
+++ main.go
@@ -1,37 +1,5 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"todo-app/repository/simple_connection"
-	"todo-app/repository/simple_sql"
-)
-
 func main() {
-	ctx := context.Background()
-	conn, err := simple_connection.CreateConnection(ctx)
-	if err != nil {
-		panic(err)
-	}
 
-	// if err := simple_sql.CreateTable(ctx, conn); err != nil {
-	// 	panic(err)
-	// }
-
-	// if err := simple_sql.InsertRow(ctx, conn, "Уборка", "ЗУБраться в комнате", false, time.Now()); err != nil {
-	// 	panic(err)
-	// }
-
-	// if err := simple_sql.UpdateRow(ctx, conn); err != nil {
-	// 	panic(err)
-	// }
-
-	// if err := simple_sql.DeleteRow(ctx, conn); err != nil {
-	// 	panic(err)
-	// }
-
-	if err := simple_sql.SelectRows(ctx, conn); err != nil {
-		panic(err)
-	}
-	fmt.Println("Success!")
 }
